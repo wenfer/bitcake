bcfd5b074a6db493280e5491b0ae9897abc096c7
jackloves <1056349644@qq.com>
2026-01-07 11:24:12 +0800

    feat: 添加Element Plus中文语言支持

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import router from './router'
 // Import Element Plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: zhCn })
 
 app.mount('#app')
